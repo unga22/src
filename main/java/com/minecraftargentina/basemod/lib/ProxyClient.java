09498248b785bff4e92d458f4de3177cfca7ed43
Tomas Eduardo Ungarini <ELPUNGA@msn.com>
2014-06-26 22:01:25 -0300

    Revert "1"
    This reverts commit 1e4f6ff421f86b8885d258b51b97e896038b3cf1.

--- main/java/com/minecraftargentina/basemod/lib/ProxyClient.java
+++
@@ -1,8 +0,0 @@
-package com.minecraftargentina.basemod.lib;
-
-public class ProxyClient extends ProxyCommon{
-	
-	public void registerRenderInformation(){
-		
-	}
-}
--- main/java/com/minecraftargentina/basemod/lib/ProxyCommon.java
+++
@@ -1,8 +0,0 @@
-package com.minecraftargentina.basemod.lib;
-
-public class ProxyCommon {
-
-	public void registerRenderInformation(){
-		
-	}
-}
