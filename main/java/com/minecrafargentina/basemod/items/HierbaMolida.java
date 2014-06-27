187ab8e9eb8b7b5a3478e9dc389da61d1c658dbd
peti446 <peti446@gmail.com>
2014-06-27 02:11:02 +0100

    Update HierbaMolida.java

--- main/java/com/minecrafargentina/basemod/items/HierbaMolida.java
+++ main/java/com/minecrafargentina/basemod/items/HierbaMolida.java
@@ -4,7 +4,7 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class HierbaMolida extends Item{
-
+	//Nuevo
 	public HierbaMolida(int i) {
 		this.setUnlocalizedName("HierbaMolida");
 		this.setTextureName("basemod:HierbaMolida");
