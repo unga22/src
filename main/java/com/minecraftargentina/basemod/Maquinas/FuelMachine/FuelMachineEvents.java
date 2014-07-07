0be2b1051d7a8bb69b9e0667a625dc0890560b81
peti446 <peti446@gmail.com>
2014-07-07 02:21:41 +0100

    Intento de Fix para el sonido fuel mashine
    consequi que ese obligado a reproducirse pero no se como hacer que dure
    ese tiempo que se este transformando

--- main/java/com/minecraftargentina/basemod/Maquinas/FuelMachine/FuelMachineEvents.java
+++ main/java/com/minecraftargentina/basemod/Maquinas/FuelMachine/FuelMachineEvents.java
@@ -4,6 +4,8 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.event.sound.PlaySoundEffectEvent;
+import net.minecraftforge.common.ISpecialArmor;
 import net.minecraftforge.event.entity.EntityEvent;
 
 import com.minecraftargentina.basemod.SoundHandler;
@@ -14,23 +16,19 @@ import com.minecraftargentina.basemod.Maquinas.CreacionDeMaquinas;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.ItemPickupEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
 
 public class FuelMachineEvents {
 	
 	private TileEntityFuelMachine fuelmashine;
-	
+	private boolean hasPlayed = false;
 	@SubscribeEvent
 	public void onEntityConstructing0(EntityEvent.EntityConstructing event) {
-	        if (TileEntityFuelMachine.prendida == true && ) {
-	 
-	     System.out.println("AAAAAAAAAAAAA");
-	        	
-	        	//  if(event.pickedUp.getEntityItem().isItemEqual(new ItemStack(CreacionDeItems.hojasDelRecuerdo))){
-	        SoundHandler.onEntityPlay("Skyrim0", event.entity.worldObj, event.entity, 1, 1);
-	        
-	        
-
-	}
+		boolean mustPlayMusic = TileEntityFuelMachine.prendida;
+	    if(mustPlayMusic == true && !hasPlayed ) {	
+	    	SoundHandler.onEntityPlay("Skyrim0", event.entity.worldObj, event.entity, 1, 1);
+	     }
+		hasPlayed = mustPlayMusic;
 	}
 	
 
