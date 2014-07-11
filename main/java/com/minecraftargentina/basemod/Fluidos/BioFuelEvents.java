0894350b021675e685be5bfb98db53adaebdd97b
Tomas Eduardo Ungarini <ELPUNGA@msn.com>
2014-07-11 01:43:21 -0300

    probando algo..

--- main/java/com/minecraftargentina/basemod/Fluidos/BioFuelEvents.java
+++ main/java/com/minecraftargentina/basemod/Fluidos/BioFuelEvents.java
@@ -13,8 +13,10 @@ import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 
 
+
 import com.minecraftargentina.basemod.BaseMod;
 import com.minecraftargentina.basemod.Achievement.CreacionDeAchievement;
+import com.minecraftargentina.basemod.Blocks.CreacionDeBloques;
 import com.minecraftargentina.basemod.Items.CreacionDeItems;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -26,66 +28,71 @@ public class BioFuelEvents {
 	@SubscribeEvent
 	public void onEventBioFuelFlint(PlayerInteractEvent event){
 		System.out.println("0");
+							
+			
+			
+
 				
-		if(event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK){
-			
-			System.out.println("1");
-			
-			
-			if(event.entityPlayer.getHeldItem() == new ItemStack(Items.flint_and_steel)){
 				
-			
-			
-			System.out.println("2");
-			
-			
-			
-			
-			int dy = event.y;
-			int dx = event.x;
-			int dz = event.z;
-			switch(event.face){
-			case 0:
-				dy = dy - 1;
-				break;
-			case 1:
-				dy = dy + 1;
-				break;
-			case 2:
-				dz = dz - 1;
-				break;
-			case 3:
-				dz = dz + 1;
-				break;
-			case 4:
-				dx = dx - 1;
-				break;
-			case 5:
-				dx = dx + 1;
-				break;
-			}
-			System.out.println(event.entityPlayer.worldObj.getBlock(event.x, event.y, event.z));
-			System.out.println(dy);
-			System.out.println(dx);
-			System.out.println(dz);
-			
-			if(event.entityPlayer.worldObj.getBlock(event.x, event.y, event.z) == CreacionDeFluidos.BiofuelBlock) {
+				System.out.println("2");
+				
+				if(event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK){
+					
+					System.out.println("3");
+					
+					
+					
+					
+					if(event.entityPlayer.worldObj.getBlock(event.x, event.y, event.z) == CreacionDeBloques.blockCopperBlock){
+						
+					
+				
 	
-				System.out.println("3");
+					
+					int dy = event.y;
+					int dx = event.x;
+					int dz = event.z;
+					
+					// Switch adds/subtracts 1 from coordinates of the block that was right clicked based on which face of the
+					// block that is associated with the PlayerInteractEvent
+					switch(event.face)
+					{
+					case 0:
+						dy -= 1;
+						break;
+					case 1:
+						dy += 1;
+						break;
+					case 2:
+						dz -= 1;
+						break;
+					case 3:
+						dz += 1;
+						break;
+					case 4:
+						dx -= 1;
+						break;
+					case 5:
+						dx += 1;
+						break;
+					}
+					
+					// e.entityPlayer.worldObj.playSoundEffect(dx + 0.5D, dy + 0.5D, dz + 0.5D, "block.wood.place", 1.0f, e.entityPlayer.worldObj.rand.nextFloat() * 0.1f + 0.9f);
 				
-				// Switch adds/subtracts 1 from coordinates of the block that was right clicked based on which face of the
-				// block that is associated with the PlayerInteractEvent
-			
-				
-				System.out.println("4");
-			
-			event.entityPlayer.worldObj.setBlock(dx, dy, dz, Blocks.grass);
-			
-	
-				
+					event.entityPlayer.worldObj.setBlock(dx, dy, dz, Blocks.grass);
+					// Places down manicMod:fence_oak instead
+					/*
+					fenceItem.placeBlockAt(e.entityPlayer.getHeldItem(), 
+							e.entityPlayer, 
+							e.entityPlayer.worldObj,
+							dx, dy, dz, 
+							e.face, 
+							0, 0, 0, 
+							0);
+					*/
+					}
 				}
+			
 			}
-	
 		}
-	}
-}
+	
