c313ae3326fa0440e20394b277d6a93d40984dac
Tomas Eduardo Ungarini <ELPUNGA@msn.com>
2014-07-11 00:37:16 -0300

    Revert "NADA AGREGADO"
    This reverts commit 9e7fc71e336d515bbdc847b0578157a0dde70e9e.

---
+++ main/java/com/minecraftargentina/basemod/Achievement/CreacionDeAchievement.java
@@ -0,0 +1,129 @@
+package com.minecraftargentina.basemod.Achievement;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import com.minecraftargentina.basemod.CraftingHandler;
+import com.minecraftargentina.basemod.GuiHandler;
+import com.minecraftargentina.basemod.Blocks.CreacionDeBloques;
+import com.minecraftargentina.basemod.Fluidos.BioFuelEvents;
+import com.minecraftargentina.basemod.Items.CreacionDeItems;
+import com.minecraftargentina.basemod.Maquinas.FuelMachine.FuelMachineEvents;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Achievement;
+import net.minecraft.stats.StatBase;
+import net.minecraft.stats.StatList;
+import net.minecraftforge.common.AchievementPage;
+import net.minecraftforge.common.MinecraftForge;
+
+public class CreacionDeAchievement {
+	//-----------------------------------------------------------------------------------------------------------------------
+	//
+	public static Achievement achievementOblivion;
+	public static Achievement achievementCopperPick;
+	
+	public static Achievement achievementHojasDelRecuerdoPick;
+	public static Achievement achievementHojasDelRecuerdoEat;
+	public static AchievementPage OblivionPage;
+	public static Achievement achievementHidden;
+	//
+	//-----------------------------------------------------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------------------------------------------------
+	//
+	public static void CreaciondeAchievement(){				
+	
+		achievementOblivion = new Achievement("archievement.Oblivion", "Oblivion", 0, 0, new ItemStack(CreacionDeItems.itemOldBook), (Achievement)null).initIndependentStat().registerStat();
+		achievementCopperPick = new Achievement("archivement.craftPick", "craftPick", 2, 1, CreacionDeBloques.oreCopperOre, achievementOblivion).registerStat();
+		achievementHojasDelRecuerdoPick = new Achievement("archievement.pickHojasDelRcuerdo", "PickHojasDelRecuerdo", 0, 4, new ItemStack(CreacionDeItems.hojasDelRecuerdo), (Achievement)null).initIndependentStat().registerStat();
+		achievementHojasDelRecuerdoEat = new Achievement("archivement.eatHojasDelRecuerdo", "EatHojasDelRecuerdo", 1, 5, CreacionDeItems.hojasDelRecuerdo, achievementHojasDelRecuerdoPick).registerStat();
+	}
+	//
+	//-----------------------------------------------------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------------------------------------------------
+	//
+	public static void RegistracionAchievement(){
+		
+		
+		OblivionPage = new AchievementPage("Oblivion Archives", new Achievement[]{achievementOblivion, achievementCopperPick, achievementHojasDelRecuerdoPick, achievementHojasDelRecuerdoEat});
+		AchievementPage.registerAchievementPage(OblivionPage);
+		
+		
+	}
+	//	
+	//-----------------------------------------------------------------------------------------------------------------------
+	public static void RegistracionDeEventos(){
+		
+		//Registro Minecraft
+		FMLCommonHandler.instance().bus().register(new OnPickHojaDelRecuerdo());
+		FMLCommonHandler.instance().bus().register(new CopperOnPickEvent());
+		FMLCommonHandler.instance().bus().register(new CopperOnCraftEvent());
+		
+		//Registro Forge
+		MinecraftForge.EVENT_BUS.register(new OnEatHojaDelRecuerdo());
+		
+       
+		
+		MinecraftForge.EVENT_BUS.register(new FuelMachineEvents());
+		FMLCommonHandler.instance().bus().register(new FuelMachineEvents());
+		
+		MinecraftForge.EVENT_BUS.register(new Hidden());
+		FMLCommonHandler.instance().bus().register(new Hidden());
+		
+		MinecraftForge.EVENT_BUS.register(new BioFuelEvents());
+		FMLCommonHandler.instance().bus().register(new BioFuelEvents());
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	public boolean addHiddenAchievement(Achievement ach) {
+	    StatList.allStats.add(ach);
+	            
+	    Map oneShotStats = null;
+	    Field field = null;
+	    
+	    for (Field f : StatList.class.getDeclaredFields()) {
+	        f.setAccessible(true);
+	        try {
+	                        if (f.getName().equals("oneShotStats") || f.getName().equals("field_75942_a")) {
+	                                oneShotStats = (Map)f.get(null);
+	                                field = f;
+	                        }
+	                } catch (Exception ex) {
+	                        System.out.println("no field " + ex);
+	                        return false;
+	                }
+	    }
+	   
+	        
+	        if (oneShotStats == null) return false;
+	        
+	        try {
+	                if (oneShotStats.containsKey(Integer.valueOf(ach.statId))) {
+	                        System.out.println("Already existing Achievement with this stat id!");
+	            return false;
+	        }
+	                
+	                oneShotStats.put(Integer.valueOf(ach.statId), ach);
+	        } catch (Exception e) {
+	                System.out.println("Failed setting");
+	                return false;
+	        }
+	        
+	        
+	    ach = new Achievement("archivement.Hidden", "Hidden", 3, 1, new ItemStack(CreacionDeBloques.blockObsidianTable), (Achievement)null).initIndependentStat();
+	        
+	    return true;
+	}
+}
---
+++ main/java/com/minecraftargentina/basemod/Blocks/CreacionDeBloques.java
@@ -0,0 +1,128 @@
+package com.minecraftargentina.basemod.Blocks;
+
+import com.minecraftargentina.basemod.BaseMod;
+import com.minecraftargentina.basemod.Biomes.OblivionLeaf;
+import com.minecraftargentina.basemod.Biomes.OblivionLog;
+import com.minecraftargentina.basemod.Biomes.OblivionSapling;
+import com.minecraftargentina.basemod.Items.ItemLeafBlocks;
+import com.minecraftargentina.basemod.Items.ItemLogBlocks;
+import com.minecraftargentina.basemod.Items.ItemSaplingBlocks;
+import com.minecraftargentina.basemod.Semillas.StrawberryCrop;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class CreacionDeBloques {
+	//-----------------------------------------------------------------------------------------------------------------------
+	//
+	public static Block oreCopperOre;
+	public static Block oreTinOre;
+	public static Block oreZincOre;
+	public static Block oreNickelOre;
+	public static Block oreManganeseOre;
+	public static Block oreVanadiumOre;
+	public static Block oreTopazOre;
+
+	public static Block oreNetherCopperOre;
+	public static Block oreNetherTinOre;
+	public static Block oreNetherZincOre;
+	public static Block oreNetherNickelOre;
+	public static Block oreNetherManganeseOre;
+	public static Block oreNetherVanadiumOre;
+	public static Block oreNetherTopazOre;
+
+	public static Block blockCopperBlock;
+	public static Block blockMinerals;
+	public static Block blockObsidianTable;
+
+	public static Block blockPurpleLampOn;
+	public static Block blockPurpleLampOff;
+
+
+	public static Block cropStrawberryPlant;
+
+	public static Block cropBloodMelonStem;
+	public static Block cropBloodMelon;
+	
+	public static Block blockLog;
+	public static Block blockLeaf;
+	public static Block blockSapling;
+	
+	public static Block blockMaderaRefinada;
+	
+	
+	
+	
+	//
+	//-----------------------------------------------------------------------------------------------------------------------
+	
+	//-----------------------------------------------------------------------------------------------------------------------
+	//
+	public static void CreaciondeBloques(){
+		cropStrawberryPlant = new StrawberryCrop().setBlockName("StrawberryPlant");
+
+		cropBloodMelon = new BloodMelon().setBlockName("BloodMelon").setBlockTextureName(BaseMod.modid + ":BloodMelon");
+		cropBloodMelonStem = new BlockNCStem(cropBloodMelon).setBlockName("BloodMelonStem").setBlockTextureName(BaseMod.modid + ":BloodMelon");
+		oreCopperOre = new OreBlock(Material.rock).setBlockName("CopperOre");
+		oreTinOre = new OreBlock(Material.rock).setBlockName("TinOre");
+		oreZincOre = new OreBlock(Material.rock).setBlockName("ZincOre");
+		oreNickelOre = new OreBlock(Material.rock).setBlockName("NickelOre");
+		oreManganeseOre = new OreBlock(Material.rock).setBlockName("ManganeseOre");
+		oreVanadiumOre = new OreBlock(Material.rock).setBlockName("VanadiumOre");
+		oreTopazOre = new OreBlock(Material.rock).setBlockName("TopazOre");
+
+		oreNetherCopperOre = new OreBlock(Material.rock).setBlockName("NetherCopperOre");
+		oreNetherTinOre = new OreBlock(Material.rock).setBlockName("NetherTinOre");
+		oreNetherZincOre = new OreBlock(Material.rock).setBlockName("NetherZincOre");
+		oreNetherNickelOre = new OreBlock(Material.rock).setBlockName("NetherNickelOre");
+		oreNetherManganeseOre = new OreBlock(Material.rock).setBlockName("NetherManganeseOre");
+		oreNetherVanadiumOre = new OreBlock(Material.rock).setBlockName("NetherVanadiumOre");
+		oreNetherTopazOre = new OreBlock(Material.rock).setBlockName("NetherTopazOre");
+		blockCopperBlock = new CopperBlock(Material.iron).setBlockName("CopperBlock");
+		blockMinerals = new MineralBlocks().setBlockName("Minerals");
+		blockObsidianTable = new ObsidianBlock(Material.rock).setBlockName("ObsidianTable");
+		blockPurpleLampOn = new PurpleLamp(true).setBlockName("PurpleLampOn");
+		blockPurpleLampOff = new PurpleLamp(false).setBlockName("PurpleLampOff").setCreativeTab(BaseMod.oblivion);
+		blockLog = new OblivionLog().setBlockName("Log").setCreativeTab(BaseMod.oblivion);
+		blockLeaf = new OblivionLeaf().setBlockName("Leaf").setCreativeTab(BaseMod.oblivion);
+		blockSapling = new OblivionSapling().setBlockName("Sapling").setCreativeTab(BaseMod.oblivion);
+		blockMaderaRefinada = new MaderaRefinada(Material.wood).setBlockName("MaderaRefinada");
+	}
+	//
+	//-----------------------------------------------------------------------------------------------------------------------
+	
+	//-----------------------------------------------------------------------------------------------------------------------
+	//
+	public static void RegistraciondeBloques(){
+		GameRegistry.registerBlock(cropStrawberryPlant, "StrawberryPlant");
+		GameRegistry.registerBlock(cropBloodMelon, "BloodMelon");
+		GameRegistry.registerBlock(cropBloodMelonStem, "BloodMelonStem");
+		GameRegistry.registerBlock(oreCopperOre, "CopperOre");
+		GameRegistry.registerBlock(oreTinOre, "TinOre");
+		GameRegistry.registerBlock(oreZincOre, "ZincOre");
+		GameRegistry.registerBlock(oreNickelOre, "NickelOre");
+		GameRegistry.registerBlock(oreManganeseOre, "ManganeseOre");
+		GameRegistry.registerBlock(oreVanadiumOre, "VanadiumOre");
+		GameRegistry.registerBlock(oreTopazOre, "TopazOre");
+
+		GameRegistry.registerBlock(oreNetherCopperOre, "NetherCopperOre");
+		GameRegistry.registerBlock(oreNetherTinOre, "NetherTinOre");
+		GameRegistry.registerBlock(oreNetherZincOre, "NetherZincOre");
+		GameRegistry.registerBlock(oreNetherNickelOre, "NetherNickelOre");
+		GameRegistry.registerBlock(oreNetherManganeseOre, "NetherManganeseOre");
+		GameRegistry.registerBlock(oreNetherVanadiumOre, "NetherVanadiumOre");
+		GameRegistry.registerBlock(oreNetherTopazOre, "NetherTopazOre");
+		GameRegistry.registerBlock(blockCopperBlock, "CopperBlock");
+		GameRegistry.registerBlock(blockPurpleLampOn, "PurpleLampOn");
+		GameRegistry.registerBlock(blockPurpleLampOff, "PurpleLampOff");
+		GameRegistry.registerBlock(blockObsidianTable, "ObsidianTable");
+		GameRegistry.registerBlock(blockLog, ItemLogBlocks.class, blockLog.getUnlocalizedName().substring(5));
+		GameRegistry.registerBlock(blockLeaf, ItemLeafBlocks.class, blockLeaf.getUnlocalizedName().substring(5));
+		GameRegistry.registerBlock(blockSapling, ItemSaplingBlocks.class, blockSapling.getUnlocalizedName().substring(5));
+		GameRegistry.registerBlock(blockMaderaRefinada, "MaderaRefinada");
+		
+	}
+	//
+	//-----------------------------------------------------------------------------------------------------------------------
+}
